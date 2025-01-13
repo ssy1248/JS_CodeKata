0f1cf087642734dcd1218188e13799c80eb6a4c4
ssy1248 <ssy1248@gmail.com>
2025-01-13 09:45:38 +0900

    25-01-13 push

---
+++ 점 찍기.js
@@ -0,0 +1,11 @@
+function solution(k, d) {
+    var answer = 0;
+    
+    for(let x = 0; x <= d; x += k) {
+        const maxY = Math.floor(Math.sqrt(d * d - x * x));
+        
+        answer += Math.floor(maxY / k) + 1;
+    }
+    
+    return answer;
+}
